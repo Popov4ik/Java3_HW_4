521edf0082157eb972d7c19994d967006ff835e0
Nikolay <stoknike@gmail.com>
2022-02-11 18:03:14 +0300

    Java3_HW_4_complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,61 @@
 public class Main {
 
+    static Object mon = new Object();
+    static volatile int currentCount = 1;
+    static final int count = 5;
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < count; i++) {
+                    synchronized (mon) {
+                        while (currentCount != 1) {
+                            mon.wait();
+                        }
+                        currentCount = 2;
+                        System.out.print("A");
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < count; i++) {
+                    synchronized (mon) {
+                        while (currentCount != 2) {
+                            mon.wait();
+                        }
+                        currentCount = 3;
+                        System.out.print("B");
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                for (int i = 0; i < count; i++) {
+                    synchronized (mon) {
+                        while (currentCount != 3) {
+                            mon.wait();
+                        }
+                        currentCount = 1;
+                        System.out.println("C");
+                        mon.notifyAll();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
 }
